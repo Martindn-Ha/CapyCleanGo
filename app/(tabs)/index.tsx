6ed29a8d10fe2145cd45587cf6d579c2459002dc
Ng <evanng21@csu.fullerton.edu>
2025-04-12 22:37:24 -0700

    take picture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
 import { StyleSheet, Button, TouchableOpacity, Alert} from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Image } from 'expo-image';
 
 export default function App() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [uri, setUri] = useState<string | null>(null);
+  const cameraRef = useRef<CameraView>(null);
+  const [previewUri, setPreviewUri] = useState(false)
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -28,15 +32,47 @@ export default function App() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  const handleCapture = async () => {
+    const photo = await cameraRef.current?.takePictureAsync();
+    if(!cameraRef.current){
+      console.error("cameraRef is not defined");
+    }
+    if (photo?.uri) {
+      setUri(photo.uri);
+      setPreviewUri(true);
+      console.log("photo", photo);
+    } else {
+      console.error("Failed to capture photo", Error);
+    }
+  };
+
+  const handleRetry = () => {
+    setUri(null);
+    setPreviewUri(false);
+  }
+
+  if(previewUri && uri){
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri }} style={styles.camera} />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.flipButton} onPress={handleRetry}>
+            <Text style={styles.flipIcon}>Retake</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.flipButton} onPress={toggleCameraFacing}>
             <FontAwesome name="refresh" style={styles.flipIcon} />
           </TouchableOpacity>
           <TouchableOpacity style={styles.captureButton}>
-            <FontAwesome name="circle" style={styles.captureIcon} />
+            <FontAwesome name="circle" style={styles.captureIcon} onPress={handleCapture} />
           </TouchableOpacity>
         </View>
       </CameraView>
